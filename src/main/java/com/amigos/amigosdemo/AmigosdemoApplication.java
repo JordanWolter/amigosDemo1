fd866750e0f1c2ce13d74a8ae1fe4c2de06586e9
Jordan Wolter <jordan.wolter@icloud.com>
2023-02-15 22:18:49 -0600

    initial commit

---
+++ src/main/java/com/amigos/amigosdemo/AmigosdemoApplication.java
@@ -0,0 +1,13 @@
+package com.amigos.amigosdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amigos/amigosdemo/AmigosdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigos.amigosdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
