585d845549a3fdb307110e79dbc5ec1e38e0bfff
Jordan Wolter <jordan.wolter@icloud.com>
2023-02-17 16:02:38 -0600

    create student class

--- src/main/java/com/amigos/amigosdemo/AmigosdemoApplication.java
+++ src/main/java/com/amigos/amigosdemo/AmigosdemoApplication.java
@@ -1,13 +1,34 @@
 package com.amigos.amigosdemo;
 
+import com.amigos.amigosdemo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class AmigosdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmigosdemoApplication.class, args);
+
 	}
 
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Jordan",
+						"jordan.wolter@icloud.com",
+						LocalDate.of(1992, Month.MAY, 13),
+						30
+				)
+		);
+	}
 }
